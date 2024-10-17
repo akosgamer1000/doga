36023ef50c071a6c508fe4838881be5397a6881c
Petrik Diak <diak@petrik.hu>
2024-10-17 13:24:31 +0200

    rwrwr

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Redirect, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { get } from 'http';
+import { error } from 'console';
+import { mozi } from './mozi.dto';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -11,5 +15,31 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+
+
+  }
+  @Get("mozifog")
+  @Render('mozif')
+  fog(){
+    return{
+      errors:[],
+      data:{}
+    }
+  }
+  @Post("mozifog")
+  moziff(@Body() data:mozi,@Res() res:Response){
+      let errors:string[]= [];
+      if(!data.datum || !data.email || !data.név || !data.nézök){
+        errors.push("mindent meg kell adini")
+      }
+      if(errors.length>0){
+          res.render('mozif',{errors,data})
+      }
+      res.redirect(303,'/succes')
+  }
+  @Get("succes")
+  @Render("succes")
+  suss(){
+
   }
 }
---
+++ src/mozi.dto.ts
@@ -0,0 +1,6 @@
+export class mozi{
+    név:string;
+    email:string;
+    datum:Date;
+    nézök:Number;
+}
